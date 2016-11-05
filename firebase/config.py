eaf47499d282b3b573ec6baf72cf7da35b6d569e
Javier López Úbeda <jlopezcur@gmail.com>
2016-11-05 06:24:34 +0100

    Firebase compatibility version

--- firebase/config.py
+++ firebase/config.py
@@ -3,8 +3,8 @@ def can_build(plat):
 
 def configure(env):
     if env['platform'] == 'android':
-        env.android_add_dependency("compile 'com.google.firebase:firebase-core:+'")
-        env.android_add_dependency("compile 'com.google.firebase:firebase-messaging:+'")
+        env.android_add_dependency("compile 'com.google.firebase:firebase-core:9.2.1'")
+        env.android_add_dependency("compile 'com.google.firebase:firebase-messaging:9.2.1'")
         env.android_add_java_dir("android")
         env.android_add_default_config("applicationId '[your-game-package]'")
         env.disable_module()
