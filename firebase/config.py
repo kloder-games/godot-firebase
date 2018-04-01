96328f3b276f889579571c37731104651117dc87
Javier López Úbeda <jlopezcur@gmail.com>
2018-04-02 00:23:41 +0200

    Config upgraded

--- firebase/config.py
+++ firebase/config.py
@@ -3,8 +3,9 @@ def can_build(plat):
 
 def configure(env):
     if env['platform'] == 'android':
-        env.android_add_dependency("compile 'com.google.firebase:firebase-core:9.2.1'")
-        env.android_add_dependency("compile 'com.google.firebase:firebase-messaging:9.2.1'")
+        env.android_add_dependency("compile 'com.google.firebase:firebase-core:9.8.0'")
+        env.android_add_dependency("compile 'com.google.firebase:firebase-messaging:9.8.0'")
+		env.android_add_dependency("compile 'com.google.firebase:firebase-crash:9.8.0'")
         env.android_add_java_dir("android")
         env.android_add_default_config("applicationId '[your-game-package]'")
         env.disable_module()
